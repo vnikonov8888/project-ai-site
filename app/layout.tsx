3795e6260f882e0c1680c106143a51d312d5ff68
vnikonov8888 <vnikonov8888@gmail.com>
2026-07-07 14:20:07 +0300

    Add Meta Pixel tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
-
+import Script from "next/script";
 const inter = Inter({ subsets: ["latin", "cyrillic"], variable: "--font-inter" });
 
 const playfair = Playfair_Display({
@@ -24,6 +24,21 @@ export default function RootLayout({
   return (
     <html lang="ru" className={`${inter.variable} ${playfair.variable}`}>
       <body className="bg-background font-sans text-white antialiased">
+        <Script id="meta-pixel" strategy="afterInteractive">
+  {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+
+    fbq('init', '1376755577661120');
+    fbq('track', 'PageView');
+  `}
+</Script>
         {children}
       </body>
     </html>
